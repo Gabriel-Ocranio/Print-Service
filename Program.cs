1eaaac0569a204b9d811f70c89f77a21d1134a95
= <gabrieleliasmega@gmail.com>
2022-11-13 14:50:21 -0300

    Program main and Print Service

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using aula_Generics.Services;
+
+PrintService<string> printService = new PrintService<string>();
+
+Console.Write("How many values? ");
+int n = int.Parse(Console.ReadLine());
+
+
+
+for (int i = 0; i < n; i++)
+{
+    string x = Console.ReadLine();
+    printService.AddValue(x);
+}
+
+printService.Print();
+
+System.Console.WriteLine("First: " + printService.First());
